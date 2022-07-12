62167c66c2a2dce65194adc50aaf8a1dec119fbf
aritra <aritra.duttas18@gmail.com>
2022-07-12 13:35:46 +0530

    jpg-to-pdf-seo-done

---
+++ handlers/handleConvertJPGtoPDFOnline.php
@@ -0,0 +1,405 @@
+<?php include 'headerHandlersCopy.php';
+session_start();
+//echo(session_id());
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/handleConvertJPGtoPDF.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+        <script src="../js/handleConvertJPGtoPDF.js"></script>
+
+
+        <title>Compressing Image</title>
+    </head>
+
+    <body>
+        <!-- Progress bar -->
+
+        <div id="wrapper">
+            <h1 id="head1">Compressing Uploaded Images</h1>
+            <h1 id="head2">Generating PDF</h1>
+            <div id="myProgress">
+                <div id="myBar">10%</div>
+            </div>
+            <br>
+        </div>
+        
+        <!-- end -->
+
+        <?php 
+      
+            //code to display errors.
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL); 
+             
+            
+            if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+                header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+                die( header( 'location: /error.php' ) );
+            }
+            
+
+            if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+                
+                $images = array();
+                $totalImages =count($_FILES['image']['tmp_name']);
+                $sessionID = session_id();
+                $saveAddress = "../upload/pdfUploads/{$sessionID}combinedPDF-geekyprofessor-App.pdf";
+                //echo($totalImages."<br>".$sessionID);
+
+
+                for( $i=0 ; $i < $totalImages ; $i++ ) {
+                    $tmpFilePath = $_FILES['image']['tmp_name'][$i];
+                    array_push($images, $tmpFilePath);
+                }
+
+                $pdf = new Imagick($images);
+                $pdf->setImageFormat('pdf');
+                try{
+                    $pdf->writeImages($saveAddress, true); 
+                    $status = 1;
+                    $statusMsg = "Images successfully converted into PDF Doc.";
+                }
+                catch(Exception $e) {
+                    echo 'Message: ' .$e->getMessage();
+                    $status = 0;
+                    $statusMsg = "Error while preparing PDF.";
+
+                }
+                $pdfFileSize = (filesize($saveAddress)/1000);
+                $tmpDownloadFile = file_get_contents($saveAddress);   
+
+                # delete files of yesterday 
+                
+                $filesToBeDeleted = glob("../upload/pdfUploads/*"); 
+                foreach($filesToBeDeleted as $file){ 
+                    if( (is_file($file)) && ( (time()-filectime($file))>=86400 ) ) {
+                    unlink($file); // delete file
+                        echo ("deleted");
+                  }
+                }
+    
+                # end of pdf file cleaning code 
+            }
+        ?>
+
+
+        <div id="result" style="display: none;">
+            <?php
+                if ($status == 0){ 
+                    echo "
+                        <div class=\"alert alert-danger\">
+                            <strong>Sorry! </strong>".$statusMsg."
+                        </div>";
+                    echo "<p id=\"statusFlag\">0</p>";
+                    echo "
+                        <div id=\"errorImage\">
+                            <img src=\"../assets/invalidExtension.jpg\" height=30% width=30%>
+                        </div>";
+                }  
+
+            ?>
+
+            <?php if(!empty($saveAddress)){ 
+                if ($status == 1) 
+                    echo "<div class=\"alert alert-success\"><strong>Success! </strong>".$statusMsg."</div>"; 
+                    echo "<p id=\"statusFlag\">1</p>";
+                ?>
+
+                
+                <div id = "resultImage">
+                    <img src = "../assets/completed1.gif" height="250px" width="200px">
+                    <p>
+                      * Cute isn't it? So are we. <br> 
+                      Your download will start automatically !!<br>
+                      All your Images have been merged to a single PDF Doc.
+                    </p>
+                </div>
+
+                <div class="alert alert-warning alert-dismissible fade show" role="alert" style="text-align: center;">
+                    <strong>Please Note!</strong> Large Images can take upto 1 minute to get converted into PDF file. Stay Strong :) 
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <a href="<?php echo $saveAddress; ?>" download>
+                    <button id="download">
+                        Download Generated PDF
+                    </button>
+                </a> 
+            <?php } ?>
+
+            <!--Start of table data -->
+
+            <table id="resultTable" class="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <th>Image Properties</th>
+                    <th>Image Values</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Images Merged in PDF</td>
+                        <td>
+                            <?php 
+                                for( $i=0 ; $i < $totalImages ; $i++ )
+                                echo (($i+1).". ".$_FILES["image"]["name"][$i]."<br>"); 
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Uploaded Image Type</td>
+                        <td><?php echo (strtoupper($_FILES["image"]["type"][0])) ?></td>
+                    </tr>
+                    <tr>
+                        <td>Converted Doc Type</td>
+                        <td><?php echo (strtoupper(pathinfo($saveAddress, PATHINFO_EXTENSION))) ?></td>
+                    </tr>
+                    <tr>
+                        <td>Converted Doc Size</td>
+                        <td><?php echo ($pdfFileSize." KB" ) ?></td>
+                    </tr>
+                </tbody>
+
+            </table>
+
+            <!--table end -->
+        </div>
+
+        <a href="../jpg-to-pdf-online.php">
+            <button id="download">Convert Another Set of Images</button>
+        </a>
+
+
+        <!-- This hidden btn will be clicked automatically by JS to download the image after 5 seconds of the page load.-->
+
+        <a href="<?php echo $saveAddress; ?>" download>
+            <button id="hiddenBtnForAutoDwnld" style="display: none;">
+                Download Auto test
+            </button>
+        </a>
+
+        <!--SEO content starts from here-->
+
+        <div id="seoContent" class="container">
+            <br>
+            <hr>
+            
+            <h2>Free JPG to PDF Converter Online Features<br>(for JPG to PDF)</h2>
+
+            <br>
+
+            <div class="row">
+              <div class="col-sm">
+                <i id="seoIcon" class="fa-solid fa-comment-dollar"></i>
+                <h3>JPG to PDF Converter Free</h3>
+
+                <p>
+                  This online JPG to PDF converter application can convert JPG to PDF for free (PDF) instantly under 10 seconds while preserving the highest possible quality and resolution of each JPEG image from the original file.     
+                </p>
+
+                <br>
+
+              </div>
+
+              <div class="col-sm">
+                <i id="seoIcon" class="fa-solid fa-person-digging"></i>
+                <h3>Convert JPG to PDF Easily</h3>
+
+                <p>
+                  With just two clicks you can convert JPG to PDF online for free into high quality single PDF Doc under 10 seconds. Just click on the Generate PDF button and let us do the rest of the hard work for you.      
+                </p>
+
+                <br>
+
+              </div>
+
+            </div>
+
+            <div class="row">
+              <div class="col-sm">
+                <i id="seoIcon" class="fa-solid fa-desktop"></i>
+                <h3>Convert JPG to PDF Cross-Platform</h3>
+
+                <p>
+                  This web-based JPG to PDF converter application works on every platform and OS you can think of. Be it Windows, Mac, Linux or Android this application works perfectly everywhere so you can convert JPG to PDF online free for Mac, Windows instantly.
+                </p>
+
+                <br>
+
+              </div>
+
+              <div class="col-sm">
+                <i id="seoIcon" class="fa-solid fa-shield-halved"></i>
+                <h3>Security Enhanced JPG to PDF Online</h3>
+
+                <p>
+                  For users content on the Internet our application uses highly encrypted techniques to convert JPG to PDF files . And delivers the converted PDF in a secure PDF file. User data is never stored in our server, it gets deleted after sometime as user exits.
+                </p>
+
+                <br>
+
+              </div>
+
+            </div>
+
+            <div class="row">
+              <div class="col-sm">
+                <i id="seoIcon" class="fa-solid fa-route"></i>
+                <h3>Convert JPG to PDF On The Way</h3>
+
+                <p>
+                  Doesn't matter wherever you are or whichever device you are using. Our JPG to PDF converter free online application is available 24*7 in the web for you to use. Convert JPG to PDF files while you commute, Walk, in office, in College etc.
+                </p>
+
+                <br>
+
+              </div>
+
+              <div class="col-sm">
+                <i id="seoIcon" class="fa-solid fa-file-zipper"></i>
+                <h3>Extract High Quality Converted PDF</h3>
+
+                <p>
+                  This web based free JPG to PDF converter application uses complex algorithm to preserve the quality of the JPEG images being converted into the PDF document. You can get the high quality single PDF doc converted into a single one instantly. Use same resolution JPEG images to get best results.
+                </p>
+
+                <br>
+
+              </div>
+
+              <hr>
+              <br>
+
+            </div>
+
+            <div class="row">
+              <div class="col-sm">
+                <i id="seoIcon" class="fa-solid fa-person-chalkboard"></i>
+                <h3>How to Convert JPG to PDF Online ? | How to Change JPG to PDF ?</h3>
+
+                <br>
+
+                <p>
+                  <strong>Step 1:</strong>
+                  Select the JPEG image files in the form above which you want to convert into PDF in this JPG to PDF converter application online.
+                </p>
+
+                <p>
+                  <strong>Step 2:</strong>
+                  Click on Generate PDF for free button and the images will upload automatically.
+                </p>
+
+                <p>
+                  <strong>Step 3:</strong>
+                  Wait for 10 seconds and let the application work to convert JPG to PDF for free.
+                </p>
+
+                <p>
+                  <strong>Step 4:</strong>
+                  After 10 seconds your converted PDF online download will begin automatically.
+                </p>
+
+                <br>
+                <hr>
+                <br>
+
+              </div>
+
+            </div>
+
+            <div class="row">
+              <div class="col-sm">
+                <i id="seoIcon" class="fa-brands fa-apple"></i>
+                <h3>How To convert JPG to PDF Files MAC</h3>
+
+                <br>
+
+                <p>
+                  <strong>Step 1:</strong>
+                  Open any Internert Browser to merge PDF online for free.
+                </p>
+
+                <p>
+                  <strong>Step 2:</strong>
+                  Visit the site apps.geekyprofessor.com
+                </p>
+
+                <p>
+                  <strong>Step 3:</strong>
+                  Navigate to the "Convert Images to PDF" section and click it.
+                </p>
+
+                <p>
+                  <strong>Step 4:</strong>
+                  Now you can upload JPG or PNG image files here to convert into PDF files for free.
+                </p>
+
+                <br>
+                <hr>
+                <br>
+
+              </div>
+
+            </div>
+
+
+
+            <div class="row">
+              <div class="col-sm">
+                <i id="seoIcon" class="fa-solid fa-hand-holding-heart"></i>
+                <h3>Why our JPG to PDF converter Online Application is Free ?</h3>
+
+                <br>
+
+                <p>
+                  As you all know JPG to PDF converter free online and all other PDF creation application of these type are not free at all. They have a daily limit on the number of JPEG imahe you can convert to PDF. They offer unlimited conversion fromJPG to PDF files in exchange of a monthly payment. 
+                </p>
+
+                <p>
+                  We understand the need of these applications for school and university students who cant afford to pay high prices for these applications. So, we made our application free of cost with unlimited JPG to PDF conversion free online.
+                </p>
+
+                <p>
+                  If you are reading this please make sure you disable the adblocker when you use the application. Because only through ads revenue it is possible to build such an application for free for you.
+                </p>
+
+                <p>
+                  At the same time please view and click the ads to support us. Have a good day. Wish you all the best.
+                </p>
+
+                <br>
+
+              </div>
+
+            </div>
+        </div>
+
+      <!--SEO content ends here-->    
+
+
+    </body>
+</html>
+
+<?php include 'footerHandlersCopy.php';?>
+
+
+
+<!-- Ref: 
+
+https://stackoverflow.com/questions/25680490/combine-jpgs-into-one-pdf-with-php#:~:text=In%20PHP%20you%20can%20use,%2D%3EwriteImages('combined.
+
+https://stackoverflow.com/questions/2704314/multiple-file-upload-in-php
+
+https://stackoverflow.com/questions/1921466/auto-delete-all-files-after-x-time#:~:text=You%20can%20use%20PHP%20core,and%20delete%20its%20file%2Ffiles.&text=It%20is%20only%20skeleton%20of%20the%20function.
+
+-->
---
+++ jpg-to-pdf-online.php
@@ -0,0 +1,275 @@
+<?php include 'header.php';
+//session_start();
+echo(session_id());
+?>
+
+<!DOCTYPE  html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>abc</title>
+    <link rel="stylesheet" href="css/convertJPGtoPDF.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    
+    <meta name="description" content="Need help to convert JPG to PDF online for free? Go nowhere. This JPG to PDF converter-free application can instantly convert JPG to PDF files while preserving the quality."/>
+
+    <meta name="keywords" content="convert jpg to pdf,jpg to pdf converter,how to convert jpg to pdf,convert from jpg to pdf,jpg to pdf free,jpg to pdf for free,for jpg to pdf,jpg to pdf,jpg to pdf iphone,jpg to pdf mac,jpg to pdf online free,jpg to pdf windows,jpg to pdf android">
+
+</head>
+
+
+<body>
+  <div class="heading">
+    <h1>Convert JPG's To PDF Online Free</h1>
+  </div>
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card">
+          <div class="card-header">
+              <div class="float-left"><strong>Please select Images here</strong> </div>
+              <div class="float-right"><strong>V 1.1 </strong> app</div>
+          </div>
+
+          <div class="card-body card-block">
+              <form id="multipleImgForm" name="mulImagesForm" action="handlers/handleConvertJPGtoPDFOnline.php" method="post" enctype="multipart/form-data" class="form-horizontal">
+                  <div class="row form-group">
+                      <div class="col-12 col-md-12">
+                          <div class="control-group" id="fields">
+                              <label class="control-label" for="field1">
+                                  Upload one or more jpg files
+                              </label>
+                              <div class="controls">
+                                  <div class="entry input-group upload-input-group">
+                                      <input id="mulImages" class="form-control" name="image[]" type="file" multiple= "multiple" required>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <p id="silentMsg">
+    Preferred Image format is JPG,JPEG. Upload same resolution Images to get best results. You can convert images to JPG <a href="convertPNGtoJPG.php">here</a> directly from our tools itself.
+  </p>
+
+  <br>
+  <br>
+  <button id="convert">Generate PDF</button>
+
+  <!--SEO content starts from here-->
+
+  <div id="seoContent" class="container">
+    <br>
+    <hr>
+    
+    <h2>Free JPG to PDF Converter Online Features<br>(for JPG to PDF)</h2>
+
+    <br>
+
+    <div class="row">
+      <div class="col-sm">
+        <i id="seoIcon" class="fa-solid fa-comment-dollar"></i>
+        <h3>JPG to PDF Converter Free</h3>
+
+        <p>
+          This online JPG to PDF converter application can convert JPG to PDF for free (PDF) instantly under 10 seconds while preserving the highest possible quality and resolution of each JPEG image from the original file.     
+        </p>
+
+        <br>
+
+      </div>
+
+      <div class="col-sm">
+        <i id="seoIcon" class="fa-solid fa-person-digging"></i>
+        <h3>Convert JPG to PDF Easily</h3>
+
+        <p>
+          With just two clicks you can convert JPG to PDF online for free into high quality single PDF Doc under 10 seconds. Just click on the Generate PDF button and let us do the rest of the hard work for you.      
+        </p>
+
+        <br>
+
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col-sm">
+        <i id="seoIcon" class="fa-solid fa-desktop"></i>
+        <h3>Convert JPG to PDF Cross-Platform</h3>
+
+        <p>
+          This web-based JPG to PDF converter application works on every platform and OS you can think of. Be it Windows, Mac, Linux or Android this application works perfectly everywhere so you can convert JPG to PDF online free for Mac, Windows instantly.
+        </p>
+
+        <br>
+
+      </div>
+
+      <div class="col-sm">
+        <i id="seoIcon" class="fa-solid fa-shield-halved"></i>
+        <h3>Security Enhanced JPG to PDF Online</h3>
+
+        <p>
+          For users content on the Internet our application uses highly encrypted techniques to convert JPG to PDF files . And delivers the converted PDF in a secure PDF file. User data is never stored in our server, it gets deleted after sometime as user exits.
+        </p>
+
+        <br>
+
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col-sm">
+        <i id="seoIcon" class="fa-solid fa-route"></i>
+        <h3>Convert JPG to PDF On The Way</h3>
+
+        <p>
+          Doesn't matter wherever you are or whichever device you are using. Our JPG to PDF converter free online application is available 24*7 in the web for you to use. Convert JPG to PDF files while you commute, Walk, in office, in College etc.
+        </p>
+
+        <br>
+
+      </div>
+
+      <div class="col-sm">
+        <i id="seoIcon" class="fa-solid fa-file-zipper"></i>
+        <h3>Extract High Quality Converted PDF</h3>
+
+        <p>
+          This web based free JPG to PDF converter application uses complex algorithm to preserve the quality of the JPEG images being converted into the PDF document. You can get the high quality single PDF doc converted into a single one instantly. Use same resolution JPEG images to get best results.
+        </p>
+
+        <br>
+
+      </div>
+
+      <hr>
+      <br>
+
+    </div>
+
+    <div class="row">
+      <div class="col-sm">
+        <i id="seoIcon" class="fa-solid fa-person-chalkboard"></i>
+        <h3>How to Convert JPG to PDF Online ? | How to Change JPG to PDF ?</h3>
+
+        <br>
+
+        <p>
+          <strong>Step 1:</strong>
+          Select the JPEG image files in the form above which you want to convert into PDF in this JPG to PDF converter application online.
+        </p>
+
+        <p>
+          <strong>Step 2:</strong>
+          Click on Generate PDF for free button and the images will upload automatically.
+        </p>
+
+        <p>
+          <strong>Step 3:</strong>
+          Wait for 10 seconds and let the application work to convert JPG to PDF for free.
+        </p>
+
+        <p>
+          <strong>Step 4:</strong>
+          After 10 seconds your converted PDF online download will begin automatically.
+        </p>
+
+        <br>
+        <hr>
+        <br>
+
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col-sm">
+        <i id="seoIcon" class="fa-brands fa-apple"></i>
+        <h3>How To convert JPG to PDF Files MAC</h3>
+
+        <br>
+
+        <p>
+          <strong>Step 1:</strong>
+          Open any Internert Browser to merge PDF online for free.
+        </p>
+
+        <p>
+          <strong>Step 2:</strong>
+          Visit the site apps.geekyprofessor.com
+        </p>
+
+        <p>
+          <strong>Step 3:</strong>
+          Navigate to the "Convert Images to PDF" section and click it.
+        </p>
+
+        <p>
+          <strong>Step 4:</strong>
+          Now you can upload JPG or PNG image files here to convert into PDF files for free.
+        </p>
+
+        <br>
+        <hr>
+        <br>
+
+      </div>
+
+    </div>
+
+
+
+    <div class="row">
+      <div class="col-sm">
+        <i id="seoIcon" class="fa-solid fa-hand-holding-heart"></i>
+        <h3>Why our JPG to PDF converter Online Application is Free ?</h3>
+
+        <br>
+
+        <p>
+          As you all know JPG to PDF converter free online and all other PDF creation application of these type are not free at all. They have a daily limit on the number of JPEG imahe you can convert to PDF. They offer unlimited conversion fromJPG to PDF files in exchange of a monthly payment. 
+        </p>
+
+        <p>
+          We understand the need of these applications for school and university students who cant afford to pay high prices for these applications. So, we made our application free of cost with unlimited JPG to PDF conversion free online.
+        </p>
+
+        <p>
+          If you are reading this please make sure you disable the adblocker when you use the application. Because only through ads revenue it is possible to build such an application for free for you.
+        </p>
+
+        <p>
+          At the same time please view and click the ads to support us. Have a good day. Wish you all the best.
+        </p>
+
+        <br>
+
+      </div>
+
+    </div>
+  </div>
+
+  <!--SEO content ends here-->
+
+  <script src="js/convertJPGtoPDF.js"></script>
+
+</body>
+
+<?php include 'footer.php';?>
+
+</html>
+
+
