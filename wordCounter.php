30f7f6986f20b5bebb40de23da4f7530604d4446
aritra <aritra.duttas18@gmail.com>
2022-07-14 02:07:51 +0530

    word-counter-redesigned

--- wordCounter.php
+++ wordCounter.php
@@ -3,50 +3,247 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <title>Compress Image Online Free - JPG, JPEG, PNG and GIF</title>
+
+	    <meta name="description" content="A simple online tool Bold text generator for writing a bold text on Facebook, Instagram, Twitter & Youtube. It converts your entered text into Unicode bold strings and copies to clipboard."/>
+
+	    <meta name="keywords" content="bold text generator,text fonts bold,bold text generator facebook,bold text generator copy and paste,bold text generator instagram,bold text generator for google forms,how to make text bold on youtube,is bold text better on iphone,tiny bold text generator">
+
         <link href="css/wordCounter.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
 	</head>
 
 	<body>
-		<div class="heading">
+		<div id="heading" class="container">
+			<br>
 			<h1>Word Counter For BLOG SEO</h1>
+			<br>
 		</div>
 
-		<div class="flex-container">
-			<div class="textareaWrapper">
-				<div class="form-group shadow-textarea">
-				  <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="16" placeholder="Write something here..." onkeyup="countWords()"></textarea>
+		<div class="container">
+			<div class="row">
+
+				<div class="col col-lg-8">
+					<div class="form-group shadow-textarea">
+					  <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="16" placeholder="Write something here..." onkeyup="countWords()"></textarea>
+					</div>
 				</div>
-				<button id="clearBtn" class="button" onclick="clearTextArea()">Clear Text</button>
+
+				<div class="col-sm">
+
+					<div class="advancedFeatures" id="adv">
+					  	<label id="advancedFeaturesLabel">Advanced Features</label>
+
+						<div class="form-group">
+						    <label for="exampleInputEmail1">Keyword Density</label>
+						    <input type="email" class="form-control" id="keyword" aria-describedby="emailHelp" placeholder="Enter Keyword e.g 'best gadgets'">
+						    <p id="writeKeywords">Keyword Analysis</p>
+						    <button class="button" onclick="countKDensity()">Calculate</button>
+					  	</div>
+
+					  	<div class="form-group">
+						    <label for="exampleInputEmail1">Stop Words Density</label>
+						    <input type="email" class="form-control" id="stopWord" aria-describedby="emailHelp" placeholder="Enter Stop Words e.g 'is', 'are', 'of'">
+						    <p id="writeStopWords">Selected Stop Word Density</p>
+						    <button class="button" onclick="countStopWordDensity()">Calculate</button>
+
+					  	</div>
+					</div>
+				</div>
+
+
 			</div>
 
+		</div>
 
-			<div class="advancedFeatures" id="adv">
-			  	<label id="advancedFeaturesLabel">Advanced Features</label>
+		<br>
 
-				<div class="form-group">
-				    <label for="exampleInputEmail1">Keyword Density</label>
-				    <input type="email" class="form-control" id="keyword" aria-describedby="emailHelp" placeholder="Enter Keyword e.g 'best gadgets'">
-				    <p id="writeKeywords">Keyword Analysis</p>
-				    <button class="button" onclick="countKDensity()">Calculate</button>
-			  	</div>
+		<div  class="container">
+			<div class="row">
+				
+				
+				<div id="output" class="col col-lg-8" style="padding:2%;">
+					<div class="row">
+						
+						<div class="col-sm">
+							<p class=wordLabel>Words:&nbsp</p>
+							<p id="wordCount">0</p>
+						</div>
 
-			  	<div class="form-group">
-				    <label for="exampleInputEmail1">Stop Words Density</label>
-				    <input type="email" class="form-control" id="stopWord" aria-describedby="emailHelp" placeholder="Enter Stop Words e.g 'is', 'are', 'of'">
-				    <p id="writeStopWords">Selected Stop Word Density</p>
-				    <button class="button" onclick="countStopWordDensity()">Calculate</button>
+						<div class="col-sm">
+							<p class="characterLabel">Characters:&nbsp</p>
+							<p id="charCount">0</p>
+						</div>
 
-			  	</div>
+					</div>
+				</div>
+				
+
 			</div>
 		</div>
 
-		<div class="output">
-			<p class=wordLabel>Words:&nbsp</p>
-			<p id="wordCount">0</p>
-			<p class="characterLabel">Characters:&nbsp</p>
-			<p id="charCount">0</p>
-		</div>
+		<br>
+		<button id="clearBtn" class="button" onclick="clearTextArea()">Clear Text</button>
+		<br>
 
+		<div id="seoContent" class="container">
+		    <br>
+		    <hr>
+		    
+		    <h2>Word Counter Online Features ( SEO Word Counter )</h2>
+
+		    <br>
+
+
+		    <div class="row">
+		      <div class="col-sm">
+		        <i id="seoIcon" class="fa-solid fa-file-word"></i>
+		        <h3 id="seoHeader">What is Online Word Counter ?</h3>
+
+		        <br>
+
+		        <p>
+					The Word Counter is a dynamic web application that counts words, characters, phrases, paragraphs, and pages in real time while also verifying spelling and grammar. It is used by writers to improve word choice and writing style, as well as to detect grammar errors and plagiarism. In addition, to help with public speaking, our application will precisely estimate speaking time to aid in preparation for class presentations, wedding speeches, and other such events.		        
+				</p>
+
+				<p>
+					Use The Word Counter by typing or pasting your data into the text space above. Word and character counts will be displayed at the top, any spelling errors will be highlighted, and your most often used keywords will be displayed on the right.
+				</p>
+
+		        <br>
+		        <hr>
+		        <br>
+
+		      </div>
+
+		    </div>
+
+		    <div class="row">
+		      <div class="col-sm">
+		        <i id="seoIcon" class="fa-solid fa-users"></i>
+		        <h3 id="seoHeader">Why use Word Counter Online ?</h3>
+
+		        <br>
+
+		        <p>
+					To begin, if you're wondering why you should use an online Word Counter Tool, try manually counting the characters in your material. Before you reach to line 8 or 9, you'll discover the practise is challenging, if not downright irritating.
+				</p>
+
+				<p>
+					And, anyway, who wants to waste valuable time counting a lengthy text when a programme like ours can readily assist you?
+				</p>
+
+				<p>
+					Meet our online counter and word finder, Count Number of Words with Geekyprofessor Apps. It is a sophisticated online word counter calculator that provides results in the blink of an eye.	        
+				</p>
+
+				<p>
+					Hold on ! we have more to offer you. Are you also into SEO ? Then no tool can provide you better keywords counter than us geekyprofessor apps. Also, you can count the stop words density in your post using geekyprofessor apps word counter.	        
+				</p>
+
+				<br>
+		        <hr>
+		        <br>
+
+		      </div>
+
+		    </div>
+
+		    <div class="row">
+		      <div class="col-sm">
+		        <i id="seoIcon" class="fa-solid fa-person-chalkboard"></i>
+		        <h3 id="seoHeader">How to use our Online Word Counter calculator ?</h3>
+
+		        <br>
+
+		        <p>
+		        	<strong>Step 1: </strong>
+	        		To use our Online Word Counter, you need to first copy text from other place to count it or find keywords using the Word Counter online.   
+		        </p>
+
+		        <p>
+		        	<strong>Step 2: </strong>
+	        		You can also write text of your own straight in the editor provided of the word counter online to count words online.
+		        </p>
+
+		        <p>
+		        	<strong>Step 3: </strong>
+		        	As you write or paste the text into our free online word counter, your text will be parsed and automatically the application will give you the word and character count.
+		        </p>
+
+		        <p>
+		        	<strong>Step 4: </strong>
+		        	If you want to find a keyword density of a particular keyword just type the keyword our free word counter will tell you its density.
+		        </p>
+
+		        <p>
+		        	<strong>Step 5: </strong>
+		        	Also, type the stop words to find its density in your post for SEO of your post or paragraph in our free online word counter.
+		        </p>
+
+				<br>
+		        <hr>
+		        <br>
+
+		      </div>
+
+		    </div>
+
+		    <div class="row">
+		      <div class="col-sm">
+		        <i id="seoIcon" class="fa-solid fa-circle-question"></i>
+		        <h3 id="seoHeader">How our free words counter online works ?</h3>
+
+		        <br>
+
+		        <p>
+					The UI of our free Word counter online application is very basic and user-friendly. While sophisticated enough to produce outstanding results, it is EXTREMELY simple to use.
+				</p>
+
+				<p>
+					In fact, all you have to do is click on this URL apps.geekyprofessor.com/word-counter-online.php and enter your content into the available text box, and the tool will automatically conduct the scan and display the results to you.
+				</p>
+
+				<br>
+		        <hr>
+		        <br>
+
+		      </div>
+
+		    </div>
+
+		    <div class="row">
+		      <div class="col-sm">
+		        <i id="seoIcon" class="fa-solid fa-shield-halved"></i>
+		        <h3 id="seoHeader">Geekyprofessor apps respects privacy of Users</h3>
+
+		        <br>
+		        <p>
+		        	Other online word Counter applications commonly keep a record of your material on their servers when you use them, but we are not like that.
+		        </p>
+
+				<p>
+					When you use our tool, or any of our other SEO and content tools, we do NOT keep or read your content.
+				</p>
+
+				<p>
+					We assure that your content is completely secure and only visible to you. You can also take advantage of our other free tools, such as <a href="apps.geekyprofessor.com/bold-text-generator.php">Bold Text Generator</a> and <a href="apps.geekyprofessor.com/jpg-to-pdf-online.php">JPG to PDF converter</a>.
+				</p>
+
+		        
+				<br>
+		        <hr>
+		        <br>
+
+		      </div>
+
+		    </div>
+
+	  	</div>
 		
 	  	<script src="js/wordCounter.js"></script>
 	</body>
